6a09d7a44482fad13205da9fe88d3db1a85114a2
chazmcgrill <charlietaylordev@gmail.com>
2017-07-07 00:08:16 +0100

    Win Status
    Created function to check win state. checks current board status
    against terminal states.

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
 
   // terminal states improved
   var terminal = [['A1','A2','A3'],['B1','B2','B3'],['C1','C2','C3'],['A1','B1','C1'],
-  ['A2','B2','C2'],['A3','B3','C3'],['A1','B2','C3'],['A3','B2','C1']];
+['A2','B2','C2'],['A3','B3','C3'],['A1','B2','C3'],['A3','B2','C1']];
   // current board status this can be compared to terminal states
   var status = { A1: '', A2: '', A3: '', B1: '', B2: '', B3: '', C1: '', C2: '', C3: '' };
 
@@ -33,7 +33,7 @@ $(document).ready(function() {
       var val = $(this).text();
       if (playerTurn && val !== 'X' && val !== 'O') {
         var section = $(this).attr('id');
-        console.log(status);
+        // console.log(status);
         update('O', section, false);
         computer();
       }
@@ -53,7 +53,7 @@ $(document).ready(function() {
 
   // update the status if comp or player
   function update(val, sqr, turn) {
-    console.log('val = ' + val + ' sqr = ' + sqr + ' turn = ' + turn);
+    // console.log('val = ' + val + ' sqr = ' + sqr + ' turn = ' + turn);
     $('#' + sqr).text(val);
     status[sqr] = val;
     playerTurn = turn;
@@ -62,12 +62,21 @@ $(document).ready(function() {
 
   // crude win checker test
   function checkWin() {
-    console.log('check win reached');
-    // wins.forEach(function(item) {
-    //   if(moveArray.sort().join('') === item) {
-    //     console.log("WIN");
-    //   }
-    // });
+    terminal.forEach(function(item) {
+      var xcount = 0, ocount = 0;
+      item.forEach(function(val) {
+        if (status[val] === 'X') {
+          xcount += 1;
+        } else if (status[val] === 'O') {
+          ocount += 1;
+        }
+      });
+      if (xcount === 3) {
+        console.log('X WIN');
+      } else if (ocount === 3) {
+        console.log('O WIN');
+      }
+    });
   }
 
   // reset button clear board and status obj
@@ -77,6 +86,7 @@ $(document).ready(function() {
       item = $(this).attr('id');
       status[item] = '';
     });
+    playerTurn = true;
   });
 
 });
